0da415d1ad3637856b6c71b469c0940fdbfee48d
Alex <alex.gauthier41@gmail.com>
2018-10-30 10:39:43 +0100

    Initialisation gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+
+
+const ROUTES: Route[] = [
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  }
+];
