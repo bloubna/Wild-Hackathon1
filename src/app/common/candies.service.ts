223042e16acdf3e6ea0d7d5e5629d639b187221c
Spac3G0at <victor.s.marie@gmail.com>
2018-10-30 15:24:24 +0100

    ajout des bonbons

---
+++ src/app/common/candies.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandiesService {
+
+  constructor(private http: HttpClient) { }
+
+  setCandies(): Observable<any> {
+    const obs = new BehaviorSubject({});
+
+    for (let i = 1; i < 6; i++) {
+      this.http.get(`https://ssl-api.openfoodfacts.org/category/candies/${i}.json`).subscribe(data => {
+        for (let j = 0; j < 20; j++) {
+          obs.next(data['products'][j]);
+        }
+      });
+    }
+
+    return obs;
+  }
+}
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CandiesService } from '../common/candies.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  products: any[] = [];
+  totalPages: number;
+
+  constructor(private service: CandiesService) {}
 
   ngOnInit() {
+
+    this.service.setCandies().subscribe(res => {
+      this.products.push(res);
+    });
+
   }
 
 }
