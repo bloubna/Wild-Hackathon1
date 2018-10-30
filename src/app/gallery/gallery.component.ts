8d07a4b1f76b466353085cd860b042f8a8a2b856
Spac3G0at <victor.s.marie@gmail.com>
2018-10-30 16:56:35 +0100

    pagination

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CandiesService } from '../common/candies.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gallery',
@@ -10,15 +12,29 @@ export class GalleryComponent implements OnInit {
 
   products: any[] = [];
   totalPages: number;
+  page: number;
+  showProducts: any[];
 
   constructor(private service: CandiesService) {}
 
   ngOnInit() {
 
-    this.service.setCandies().subscribe(res => {
+    this.page = 1;
+
+    this.service.setCandies()
+    .subscribe(res => {
       this.products.push(res);
+      this.totalPages = this.products.length;
+      if (this.totalPages > 20) {
+        this.showProducts = this.products.slice(0, 12);
+      }
     });
 
+  }
+
+  change() {
+    console.log(this.page);
+    this.showProducts = this.products.slice((this.page - 1) * 12, 12 * this.page);
   }
 
 }
